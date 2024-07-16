cfb16d055a69fa908ce0fd641186fd25f8b4760e
Özcan <ozcanozdemir001@gmail.com>
2024-07-16 15:10:30 +0300

    inital commit

---
+++ FirstSpriteKitTry/GameScene.swift
@@ -0,0 +1,54 @@
+//
+//  GameScene.swift
+//  FirstSpriteKitTry
+//
+//  Created by Özcan on 9.07.2024.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+  
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
